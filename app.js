6e811f5effe1f27c45b9e1667573700cc787f186
Konstantin <68119580+Konshtantin@users.noreply.github.com>
2021-09-29 17:27:11 +0300

    nnnn

--- app.js
+++ app.js
@@ -51,11 +51,7 @@ app.post('/evl',
         num = num.slice(0, j) + 'n' + num.slice(j)
         i = j+2
     }
-    let result = ''
-    if(num === undefined) {
-        result = 0
-    } else {
-        result = eval(num).toString()
+    let result = eval(num).toString()
     res.json({result: result, exp: req.body.exp})
 })
 
